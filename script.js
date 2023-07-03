c309b6de1eea787bd4d98d9e34743922898a23ce
Aditya <121721549+adityaSalame@users.noreply.github.com>
2023-07-03 13:09:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,46 +1,38 @@
 // Your Script here.
+function fun(ch){
+		
+	let a="";
+	if(ch>='A' && ch<='M')
+	a=String.fromCharCode(ch.charCodeAt(0)+13);
+	else{
+	a=String.fromCharCode((ch.charCodeAt(0))-78+65);
+	}
+	
+	return a;
+}
 
-const lookup = {
-  A: "N",
-  B: "O",
-  C: "P",
-  D: "Q",
-  E: "R",
-  F: "S",
-  G: "T",
-  H: "U",
-  I: "V",
-  J: "W",
-  K: "X",
-  L: "Y",
-  M: "Z",
-  N: "A",
-  O: "B",
-  P: "C",
-  Q: "D",
-  R: "E",
-  S: "F",
-  T: "G",
-  U: "H",
-  V: "I",
-  W: "J",
-  X: "K",
-  Y: "L",
-  Z: "M",
-  "?": "?",
-  ",": ",",
-};
 
 function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
+	let ans="";
 
-  return; //return decodedArr
+	
+for(let i=0;i<encodedStr.length;i++){
+	let ch=encodedStr.charAt(i);
+	if(ch>='A' && ch<='Z')//decodedArr.push(fun(ch))
+	ans=ans+fun(ch);
+	else ans=ans+ch;
+}
+	//document.write(ans+""+typeof(decodedArr));
+	decodedArr.push(ans);
+  return decodedArr;//decodedArr; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-//console.log(rot13("SERR YBIR? NPPVBWBO"));
+document.write(rot13("SERR YBIR? NPPVBWBO"));
 
 // Do not change this line
+
 window.rot13 = rot13;
